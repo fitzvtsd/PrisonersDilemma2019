7cbd5800177da6cc29c4654b4d0000968f0d39e1
fitzvtsd <jeremy.fitzpatrick@vtsd.com>
2019-12-12 07:36:41 -0500

    Update team1.py

--- team1.py
+++ team1.py
@@ -1,4 +1,4 @@
-####
+ ####
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
@@ -6,11 +6,13 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = '' # Only 10 chars displayed.
+team_name = 'salvemundi' # Only 10 chars displayed.
 strategy_name = ''
 strategy_description = ''
     
 def move(my_history, their_history, my_score, their_score):
+    if len(my_history)==0: #It's the first round; collude.
+        return 'c'    
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
